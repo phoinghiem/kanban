fcbbf2f2957ba51c1339eb4253e3b7b01c64bf71
Joanne <phoinghi.n.nghiem@student.uts.edu.au>
2024-02-28 13:20:50 +1100

    adding in controlling for getTasks,getTask and createTask

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,58 @@
+const Task = require('../models/taskModel')
+const mongoose = require('mongoose')
+
+// GET all tasks
+const getTasks = async (req, res) => {
+    // find the latest results
+    const tasks = await Task.find({}).sort({createdAt: -1})
+
+    res.status(200).json(tasks)
+}
+// GET a new task
+
+const getTask = async (req, res) => {
+    // find the latest results
+    const { id } = req.params
+
+
+    // checks if ID is valid and if it is not then if statement will return a 404 error
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({error: "no such task exist"})
+    }
+
+    const task = await Task.findById(id)
+    //if task is not found then return error but if found then return status 200 = good
+    if (!task) {
+        return res.status(400).json({error: "no such task exist"})
+    }
+
+    res.status(200).json(task)
+   
+}
+
+
+// CREATE a new task
+const createTask = async (req, res) => {
+    const {title, reps, load} = req.body
+
+    // add dot to DB
+    try {
+        const task = await Task.create({title, reps, load})
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+// DELETE a task 
+
+
+// UPDATE a task
+
+
+module.exports = {
+    getTasks,
+    getTask,
+    createTask
+}
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,39 +1,32 @@
 const express = require('express')
 const Task = require('../models/taskModel')
+const {
+    getTasks,
+    getTask,
+    createTask
+} = require('../controllers/taskController')
 
 const router = express.Router()
 
 
 // Creating different routes for each action
+
 // GET all tasks
-router.get("/", (req, res) => {
-    res.json({mssg: "GET all tasks"})
-})
+router.get('/', getTasks)
 
 // GET single task (route parameter ==> "/:id")
-router.get("/:id", (req, res) => {
-    res.json({mssg: "GET a single task"})
-})
+router.get('/:id', getTask)
 
 // POST new task
-router.post("/", async (req, res) => {
-    const {title, reps, load} = req.body
-    try {
-        const task = await Task.create({title, reps, load})
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-    //res.json({mssg: "POST a new single task"})
-})
+router.post('/', createTask)
 
 // DELETE new task
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
     res.json({mssg: "DELETE a new single task"})
 })
 
 // UPDATE new task
-router.patch("/:id", (req, res) => {
+router.patch('/:id', (req, res) => {
     res.json({mssg: "UPDATE a single task"})
 })
 
